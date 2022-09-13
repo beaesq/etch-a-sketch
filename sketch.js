b62dec9e1eb25938c4458fdacec5a302621b95c3
Bwa <40144023+beaesq@users.noreply.github.com>
2022-09-14 03:29:12 +0800

    Add New Sketch, Change Grid Size

--- sketch.js
+++ sketch.js
@@ -26,12 +26,49 @@ function changeSquareColor (id) {
 
 function getRandomColor () {
   return '#' + Math.floor(Math.random()*16777215).toString(16);
-
 }
+
+function resetSketchpadColor () {
+  const allSquares = document.querySelectorAll('.square');
+  allSquares.forEach(e => e.style.backgroundColor = 'white');
+}
+
+function toggleColor () {
+  color = !color;
+  resetSketchpadColor();
+  const div = document.querySelector('#divChangeColor');
+  div.textContent = color ? 'RANDOM' : 'BLACK';
+}
+
+function setGridSize () {
+  let newSize = 0;
+  
+  do {
+    newSize = parseInt(prompt('Set new grid size (1-100)', 16));
+  } while (newSize > 100 || newSize < 1);
+  
+  clearSketchpad();
+  resetSketchpadColor();
+  makeSketchpad (newSize);
+  startSketchpad();
+}
+
+function clearSketchpad () {
+  const allSquares = document.querySelectorAll('.square');
+  allSquares.forEach(e => e.remove());
+  const allRows = document.querySelectorAll('.row');
+  allRows.forEach(e => e.remove());
+}
+
+function startSketchpad () {
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(sqr => sqr.addEventListener('mouseover', (e)=>changeSquareColor(e.target.id), false));
+}
+
 let color = false;
-let res = 16;
+let initRes = 16;
 
-makeSketchpad(res);
+makeSketchpad(initRes);
+startSketchpad();
 
-const squares = document.querySelectorAll('.square');
-squares.forEach(sqr => sqr.addEventListener('mouseover', (e)=>changeSquareColor(e.target.id), false));
+
